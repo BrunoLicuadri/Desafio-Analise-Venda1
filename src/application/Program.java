11d3a87e4b96a8088a6b59c65c849e84e6a73dc0
Bruno Licuadri <blicuadri@gmail.com>
2022-10-09 10:25:57 -0300

    First steps on Challenge Sales Analisys 1

---
+++ src/application/Program.java
@@ -0,0 +1,64 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Collector;
+
+import entities.Sale;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		List<Sale> sale = new ArrayList<>();
+
+		System.out.print("Entre o caminho do arquivo: ");
+		String path = sc.nextLine();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			;
+
+			String line = br.readLine();
+
+			while (line != null) {
+				String[] fields = line.split(",");
+				sale.add(new Sale(Integer.parseInt(fields[0]), Integer.parseInt(fields[1]), fields[2],
+						Integer.parseInt(fields[3]), Double.parseDouble(fields[4])));
+				line = br.readLine();
+			}
+
+			System.out.println("Cinco primeiras vendas de 2016 de maior preço médio: ");
+
+			Comparator<String> comp = (S1, S2) -> S1.toUpperCase().compareTo(S2.toUpperCase());
+
+			List<String> pm = sale.stream().filter(x -> x.getAveragePrice()).sorted(comp.reversed()).limit(5)
+					.collect(Collector.toSale());
+
+			pm.forEach(System.out::println);
+
+			double sum = sale.stream()
+					.filter(x -> x.getSeller("Logan"))
+					.filter(x -> x.getMonth(1,7))
+					.map(x -> x.getPrice())
+					.reduce(0.0, (x, y) -> x + y);
+
+			System.out.println("Valor total vendido pelo vendedor Logan nos meses 1 e 7 = " + sum);
+
+		} catch (
+
+		IOException e) {
+			System.out.println("Erro: " + path + " (O sistema não pode encontrar o arquivo especificado)");
+		}
+
+		sc.close();
+	}
+}
